002287427843ef3bfb5096f4e7b3ced17da6b0f4
shouhei-yamamoto <elefish19920509@gmail.com>
2022-02-25 10:04:55 +0900

    seedの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-50.times do |n|
+3.times do |n|
   name = Faker::Games::Pokemon.name
   email = Faker::Internet.email
   password = "password"
