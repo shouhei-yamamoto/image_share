6bceda353b5aaa529c0519fad639d4d2b9eecbe4
shouhei-yamamoto <elefish19920509@gmail.com>
2022-02-28 01:17:02 +0900

    edit、css修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
+  before_action :check_user, only: [:edit, :update, :destroy]
   
   def new
     @user = User.new
@@ -17,10 +18,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    if @user != current_user
-      flash[:caution] = '編集権限がありません' 
-      redirect_to edit_user_path(@user.id)   
-    end
   end
 
   def edit
@@ -49,5 +46,13 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation, :image, :image_cache)
+  end
+
+  def check_user
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      flash[:danger] = "編集権限がありません!"
+      redirect_back(fallback_location: @user)
+    end
   end
 end
